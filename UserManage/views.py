fb2bba06111c04f4ede4e9bf3c1a23908cd40a40
TT-LAPTOP\htaoj <htaoji1988@126.com>
2023-04-07 20:57:09 +0800

    202304072057

--- UserManage/views.py
+++ UserManage/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from UserManage.models import User, RoleList, UserManager
+from django.db.models import Q
+from UserManage.models import User, RoleList, UserManager, PermissionList
 import json
 import logging
 import datetime
@@ -114,9 +115,9 @@ def list_user(request):
         kwargs['nickname__contains'] = nickname.strip()
     if role__name and role__name != 'all':
         kwargs['role__name'] = role__name.strip()
-    if is_active == "true":
+    if is_active == 'true':
         kwargs['is_active'] = True
-    elif is_active == "false":
+    elif is_active == 'false':
         kwargs['is_active'] = False
 
     objs = User.objects.filter(**kwargs).values('id', 'username', 'email', 'is_active', 'nickname', 'role__name',
@@ -182,7 +183,7 @@ def del_user(request):
         except Exception as e:
             result = {
                 'success': 'False',
-                'log': e
+                'log': f'{e}'
             }
 
         return JsonResponse(result)
@@ -197,3 +198,192 @@ def roles(request):
         res[o['name']] = {"text": o['name']}
 
     return JsonResponse(res)
+
+
+@csrf_exempt
+def role_list(request):
+    logger = logging.getLogger('user_manage')
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    logger.info(body)
+    role_name = body.get('name', '').strip()
+
+    kwargs = {}
+    if role_name:
+        kwargs['name__contains'] = role_name
+
+    objs = RoleList.objects.filter(**kwargs).values('id', 'name')
+
+    res = {'data': list(objs)}
+
+    return JsonResponse(res)
+
+
+@csrf_exempt
+def add_role(request):
+    logger = logging.getLogger('user_manage')
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        del body['id']
+        logger.info(f'add role!\n{body}')
+        role_name = body.get('name').strip()
+
+        exsit = RoleList.objects.filter(name=role_name)
+        if not exsit:
+            try:
+                new_user = RoleList.objects.create(name=role_name)
+            except Exception as e:
+                result = {
+                    'success': 'False',
+                    'log': f'{e}'
+                }
+                return JsonResponse(result)
+        else:
+            result = {
+                'success': 'False',
+                'log': "角色已存在!"
+            }
+            return JsonResponse(result)
+
+    result = {
+        'success': 'True',
+        'log': ''
+    }
+
+    return JsonResponse(result)
+
+
+@csrf_exempt
+def del_role(request):
+    logger = logging.getLogger('user_manage')
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        logger.info(f'del role!\n{body}')
+        id = body.get('id')
+        try:
+            RoleList.objects.filter(id=id).delete()
+            result = {
+                'success': 'True',
+                'log': ''
+            }
+        except Exception as e:
+            result = {
+                'success': 'False',
+                'log': f'{e}'
+            }
+
+        return JsonResponse(result)
+
+
+@csrf_exempt
+def permissions(request):
+    logger = logging.getLogger('user_manage')
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    logger.info(body)
+    name = body.get('name', '').strip()
+    url = body.get('url', '').strip()
+
+    kwargs = {}
+    if name:
+        kwargs['name__contains'] = name
+    if url:
+        kwargs['url__contains'] = url
+
+    objs = PermissionList.objects.filter(**kwargs).values('id', 'name', 'url')
+
+    res = {'data': list(objs)}
+
+    return JsonResponse(res)
+
+
+@csrf_exempt
+def add_permission(request):
+    logger = logging.getLogger('user_manage')
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        del body['id']
+        logger.info(f'add permission!\n{body}')
+        name = body.get('name').strip()
+        url = body.get('url').strip()
+
+        exsit = PermissionList.objects.filter(Q(name=name) | Q(url=url))
+        if not exsit:
+            try:
+                permission = PermissionList.objects.create(name=name, url=url)
+            except Exception as e:
+                result = {
+                    'success': 'False',
+                    'log': f'{e}'
+                }
+                return JsonResponse(result)
+        else:
+            result = {
+                'success': 'False',
+                'log': "名称或者url已经存在不能重复添加!"
+            }
+            return JsonResponse(result)
+
+    result = {
+        'success': 'True',
+        'log': ''
+    }
+
+    return JsonResponse(result)
+
+
+@csrf_exempt
+def del_permission(request):
+    logger = logging.getLogger('user_manage')
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        logger.info(f'del permission!\n{body}')
+        id = body.get('id')
+        try:
+            PermissionList.objects.filter(id=id).delete()
+            result = {
+                'success': 'True',
+                'log': ''
+            }
+        except Exception as e:
+            result = {
+                'success': 'False',
+                'log': f'{e}'
+            }
+
+        return JsonResponse(result)
+
+
+@csrf_exempt
+def update_permission(request):
+    logger = logging.getLogger('user_manage')
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        logger.info(f'update permission!\n{body}')
+        id = body.get('id')
+        name = body.get('name').strip()
+        url = body.get('url').strip()
+
+        exsit = PermissionList.objects.exclude(id=id).filter(Q(name=name) | Q(url=url))
+        if not exsit:
+            try:
+                permission = PermissionList.objects.filter(id=id).update(name=name, url=url)
+            except Exception as e:
+                result = {
+                    'success': 'False',
+                    'log': f'{e}'
+                }
+                return JsonResponse(result)
+        else:
+            result = {
+                'success': 'False',
+                'log': "更新失败名称或者url已经存在!"
+            }
+            return JsonResponse(result)
+
+    result = {
+        'success': 'True',
+        'log': ''
+    }
+
+    return JsonResponse(result)
